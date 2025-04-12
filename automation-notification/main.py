836abf1410d5205667e0f9eb885463b5bfeb27d6
Muhammad Satrio Wicaksono <muhsatrio@gmail.com>
2025-04-12 11:16:46 +0700

    refactor

--- automation-notification/main.py
+++
@@ -1,9 +0,0 @@
-from time import sleep
-from random import random
-from service import notification_service
-
-while (True):
-    is_dangerous = random()
-    if (is_dangerous > 0.8):
-        notification_service.send()
-    sleep(5)
